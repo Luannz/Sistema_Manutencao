e079e8a62e3100077d6e655424fbc471e2ea7bd4
Luannz <luanncc2014@gmail.com>
2026-05-26 10:03:35 -0300

    mudando o debug pra false

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-^#gc)2kzl@=99od4bc4#&ck+ff_wh8eaf%!8c%t632rj^hn_j&')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 
 ALLOWED_HOSTS = [
